8581c6b3d24a1277d641e427ba630cb0825f7b39
abrigoni <brian.rigoni1@gmail.com>
2025-07-29 03:55:07 +0200

    reset config

--- tmux.sh
+++ tmux.sh
@@ -1,3 +1,2 @@
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s tmux/tmux.reset.conf ~/.config/tmux/tmux.reset.conf
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s tmux/tmux.conf ~/.config/tmux/tmux.conf
